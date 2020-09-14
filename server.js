afac8f36e2dc154812d651fd273a988142a3b7e7
unknown <anton13jeejo@gmail.com>
2020-09-14 14:20:47 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,55 @@
 const mongoose = require("mongoose");
-const client = require("socket.io").listen(4000).sockets;
+const socketio = require("socket.io");
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
+const client = socketio(server);
+const roomSchema = new mongoose.Schema({
+  roomName: String,
+  img: String,
+});
+
+let tempRoom = new mongoose.model("rooms", roomSchema);
+app.get("/", (req, res) => {
+  tempRoom.aggregate([{ $sample: { size: 12 } }]).then((result) => {
+    return res.status(200).send(result);
+  });
+});
+
+app.get("/:roomName", (req, res) => {
+  tempRoom
+    .find({ roomName: req.params.roomName })
+    .then((result) => {
+      if (result.length > 0) {
+        return res.status(200).send({ success: true, data: result[0] });
+      } else {
+        return res.send({ success: false });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).send({ success: false });
+    });
+});
+
+app.post("/", (req, res) => {
+  tempRoom
+    .create({ roomName: req.body.roomName, img: req.body.img })
+    .then(() => {
+      return res.status(200).send({ success: true });
+    })
+    .catch((err) => {
+      return res.status(500).send({ success: false });
+    });
+});
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server has started.`));
 
 //Connect to mongo
 mongoose
@@ -11,52 +61,49 @@ mongoose
         throw err;
       }
       console.log("MongoDB conencted..");
-
+      const schema = new mongoose.Schema({
+        name: String,
+        message: String,
+      });
       //Connect to Socket.io
       client.on("connection", (socket) => {
-        let chat = new mongoose.model("chats", {
-          name: String,
-          message: String,
-        });
         //Create a function to send status
-        const sendStatus = (s) => {
-          socket.emit("status", s);
-        };
-
-        //Get chats from db
-
+        tempRoom.aggregate([{ $sample: { size: 9 } }]).then((res) => {
+          socket.emit("rooms", res);
+        });
+        let roomName = Object.values(socket.request._query)[0];
+        let roomImg = Object.values(socket.request._query)[1];
+        let chat = new mongoose.model(roomName, schema);
         chat
           .find({})
+          .limit(50)
           .then((res) => {
             //Emit the messages
-            socket.emit("output", res);
+            if (res.length > 0) {
+              socket.emit("output", res);
+            }
           })
           .catch((err) => {
             throw err;
           });
 
+        //Get chats from db
+
         //Handle input events
         socket.on("input", (data) => {
-          let name = data.name;
           let message = data.message;
 
           //Check for name and the message
-          if (name == "" || message == "") {
+          if (message.trim() == "") {
             //Send error status
 
-            sendStatus("Please enter a name and message");
+            return;
           } else {
             //Insert message
             chat
-              .create({ name: name, message: message })
+              .create({ message: message })
               .then((data) => {
                 client.emit("output", [data]);
-
-                //Send status object
-                sendStatus({
-                  message: "Message sent",
-                  clear: true,
-                });
               })
               .catch((err) => {
                 throw err;
@@ -69,7 +116,6 @@ mongoose
           chat
             .remove({})
             .then(() => {
-              // Emit cleared
               socket.emit("cleared");
             })
             .catch((err) => {
